a5d38e6ab3b34135c4daa16ea9dd060b389d5ee9
lkers <laura.kerssens@safe.com>
2014-07-28 12:59:48 -0700

    Updated CallGeocoder workspace

--- www/js/easyGeocoder.js
+++ www/js/easyGeocoder.js
@@ -1,230 +1,229 @@
-$(document).ready(function(){
-	
-	var fileLocation = '';
-	document.getElementById('dropdowns').style.display = 'none';
-	document.getElementById('mapPage').style.display = 'none';
-	
-	//initialize behaviour for file upload 
-	$('#fileupload').fileupload({
-		url : 'https://fmepedia2014-safe-software.fmecloud.com/fmedataupload/EasyGeocoder/GenerateSchemaElements.fmw?opt_fullpath=true',
-		dropzone : $('#dropzone'),
-		autoUpload : true,
+$(document).ready(function() {
 
-		add: function (e, data) {
-			data.submit();
-    	},
+    document.getElementById('dropdowns').style.display = 'none';
+    document.getElementById('mapPage').style.display = 'none';
 
-		done : function(e, data){
-			//when file has been uploaded, capture JSON response from FME Server
-			//and use file path to run workspace
-			filePath = data.result.serviceResponse.files.file[0].path;
+    //initialize behaviour for file upload 
+    $('#fileupload').fileupload({
+        url : 'https://fmepedia2014-safe-software.fmecloud.com/fmedataupload/EasyGeocoder/GenerateSchemaElements.fmw?opt_fullpath=true',
+        dropzone : $('#dropzone'),
+        autoUpload : true,
 
-			geocoder.requestSchema(filePath);
-		},
+        add: function (e, data) {
+            data.submit();
+        },
 
-		fail : function(e, data){
-			//do something if there was an error in uploading
-		},
+        done : function(e, data) {
+            //when file has been uploaded, capture JSON response from FME Server
+            //and use file path to run workspace
+            var filePath = data.result.serviceResponse.files.file[0].path;
 
-		dragover : function(e, data){
-			//make dropzone prettier
-			var dropZone = $('#dropzone');
-			var timeout = window.dropZoneTimeout;
+            geocoder.requestSchema(filePath);
+        },
 
-				if (!timeout){
-					dropZone.addClass('in');
-				}
-				else{
-					clearTimeout(timeout);
-				}
+        fail : function(e, data){
+            //do something if there was an error in uploading
+        },
 
-				var found = false;
-				var node = e.target;
-				do {
-					if (node == dropZone[0]){
-						found = true;
-						break;
-					}
-					node = node.parentNode;
-				}
-				while (node != null);
-				if (found){
-					dropZone.addClass('hover');
-				}
-				else {
-					dropZone.removeClass('hover');
-				}
-				window.dropZoneTimeout = setTimeout(function(){
-					window.dropZoneTimeout = null;
-					dropZone.removeClass('in hover');
-				}, 100);
-		}
-	});
+        dragover : function(e, data) {
+            //make dropzone prettier
+            var dropZone = $('#dropzone');
+            var timeout = window.dropZoneTimeout;
 
-	geocoder.init({
-		host : 'https://fmepedia2014-safe-software.fmecloud.com',
-		token : 'fb1c3ee6828e6814c75512dd4770a02e73d913b8'
-	});	
+            if (!timeout) {
+                dropZone.addClass('in');
+            } 
+            else {
+                clearTimeout(timeout);
+            }
+
+            var found = false;
+            var node = e.target;
+            do {
+                if (node === dropZone[0]) {
+                    found = true;
+                    break;
+                }
+                node = node.parentNode;
+            } 
+            while (node !== null);
+            if (found) {
+                dropZone.addClass('hover');
+            } 
+            else {
+                dropZone.removeClass('hover');
+            }
+            window.dropZoneTimeout = setTimeout(function() {
+                window.dropZoneTimeout = null;
+                dropZone.removeClass('in hover');
+            }, 100);
+        }
+    });
+
+    geocoder.init({
+        host : 'https://fmepedia2014-safe-software.fmecloud.com',
+        token : 'fb1c3ee6828e6814c75512dd4770a02e73d913b8'
+    }); 
 
 }); 
 
-var geocoder = (function(){
+var geocoder = (function() {
 
-	//private
-	var schemaWorkspace = 'GenerateSchemaElements.fmw';
-	var geocodeWorkspace = 'CallGeocoder.fmw';
-	var repository = 'EasyGeocoder';
-	var host;
-	var token;
-	var layer, map;
-	var loading;
+    //private
+    var schemaWorkspace = 'GenerateSchemaElements.fmw';
+    var geocodeWorkspace = 'CallGeocoder.fmw';
+    var repository = 'EasyGeocoder';
+    var host;
+    var token;
+    var layer, map;
+    var loading;
 
-	function createComboBox(boxName, colList){
-		var html = '<select id="' + boxName + '">';
-		//populate selection choices
-		var colCount = colList.columns.length;
-		html = html + '<option selected="true" style="display:none;" value=" ">Choose a Field</option>'
-		html = html + '<option value=" "> --N/A-- </option>';
-		for (i = 0; i< colCount; i++){
-			html = html + '<option value="' + colList.columns[i] + '">' + colList.columns[i] + '</option>';
-		}
-		html = html + '</select>';
-		return html;
-	}
+    function createComboBox(boxName, colList) {
+        var html = '<select id="' + boxName + '">';
+        //populate selection choices
+        var colCount = colList.columns.length;
+        html = html + '<option selected="true" style="display:none;" value=" ">Choose a Field</option>'
+        html = html + '<option value=" "> --N/A-- </option>';
+        for (i = 0; i< colCount; i++){
+            html = html + '<option value="' + colList.columns[i] + '">' + colList.columns[i] + '</option>';
+        }
+        html = html + '</select>';
+        return html;
+    }
 
-	function displayNextStep(colList){
-		document.getElementById('content').style.display = 'none';
-		document.getElementById('dropdowns').style.display = 'block';
+    function displayNextStep(colList) {
+        document.getElementById('content').style.display = 'none';
+        document.getElementById('dropdowns').style.display = 'block';
 
-		//take the list of csv columns and create a set of dropdown menus
-		//to set the parameters for the next workspace
-		document.getElementById("address").innerHTML = createComboBox("Address_field", colList);
-		document.getElementById("city").innerHTML = createComboBox("City_field", colList);
-		document.getElementById("state").innerHTML = createComboBox("StateProvince_field", colList);
-		document.getElementById("postalcode").innerHTML = createComboBox("PostalCode_field", colList);
-		document.getElementById("country").innerHTML = createComboBox("Country_field", colList);		
-	}
+        //take the list of csv columns and create a set of dropdown menus
+        //to set the parameters for the next workspace
+        document.getElementById("address").innerHTML = createComboBox("Address_field", colList);
+        document.getElementById("city").innerHTML = createComboBox("City_field", colList);
+        document.getElementById("state").innerHTML = createComboBox("StateProvince_field", colList);
+        document.getElementById("postalcode").innerHTML = createComboBox("PostalCode_field", colList);
+        document.getElementById("country").innerHTML = createComboBox("Country_field", colList);        
+    }
 
-	function getParams(){
+    function getParams() {
 
-		var address = document.getElementById("Address_field").value;
-		var city = document.getElementById("City_field").value;
-		var province = document.getElementById("StateProvince_field").value;
-		var postalcode = document.getElementById("PostalCode_field").value;
-		var country = document.getElementById("Country_field").value;
+        var address = document.getElementById("Address_field").value;
+        var city = document.getElementById("City_field").value;
+        var province = document.getElementById("StateProvince_field").value;
+        var postalcode = document.getElementById("PostalCode_field").value;
+        var country = document.getElementById("Country_field").value;
 
-		var params = 'SourceDataset=' + fileLocation;
-		params = params + '&AddressAttr=' + address;
-		params = params + '&CityAttr=' + city;
-		params = params + '&ProvinceAttr=' + province;
-		params = params + '&PostalCodeAttr=' + postalcode;
-		params = params + '&CountryAttr=' + country;
+        var params = 'SourceDataset=' + fileLocation;
+        params = params + '&AddressAttr=' + address;
+        params = params + '&CityAttr=' + city;
+        params = params + '&ProvinceAttr=' + province;
+        params = params + '&PostalCodeAttr=' + postalcode;
+        params = params + '&CountryAttr=' + country;
 
-		var encodedParams = encodeURI(params);
-			
-		return encodedParams;
-	}
+        var encodedParams = encodeURI(params);
+            
+        return encodedParams;
+    }
 
-	function clearDropdowns(){
-		document.getElementById('address').innerHTML = '';
-		document.getElementById('city').innerHTML = '';
-		document.getElementById('state').innerHTML = '';
-		document.getElementById('postalcode').innerHTML = '';
-		document.getElementById('country').innerHTML = '';
-	}
+    function clearDropdowns() {
+        document.getElementById('address').innerHTML = '';
+        document.getElementById('city').innerHTML = '';
+        document.getElementById('state').innerHTML = '';
+        document.getElementById('postalcode').innerHTML = '';
+        document.getElementById('country').innerHTML = '';
+    }
 
-	function initGoogleMap(){
-		//init google maps
-		loading = new Image();
-		loading.src = "libs/upload/img/loading.gif";
-		loading.id = "loadingImg";
-		
-		//google maps init
-		var mapStyles = [ {
-			featureType : "all",
-			elementType : "labels",
-			stylers : [ { visibility : "off" } ]
-		}];
+    function initGoogleMap() {
+        //init google maps
+        loading = new Image();
+        loading.src = "libs/upload/img/loading.gif";
+        loading.id = "loadingImg";
+        
+        //google maps init
+        var mapStyles = [ {
+            featureType : "all",
+            elementType : "labels",
+            stylers : [ { visibility : "off" } ]
+        }];
 
-		var mapOptions = {
-			zoom: 3,
-			center: new google.maps.LatLng( 50.355, -97.855 ),
-			mapTypeId : google.maps.MapTypeId.SATELLITE,
-			disableDefaultUI : true
-		};
+        var mapOptions = {
+            zoom: 3,
+            center: new google.maps.LatLng( 50.355, -97.855 ),
+            mapTypeId : google.maps.MapTypeId.SATELLITE,
+            disableDefaultUI : true
+        };
 
-		map = new google.maps.Map( document.getElementById( "mapDiv" ), mapOptions );
+        map = new google.maps.Map( document.getElementById( "mapDiv" ), mapOptions );
 
-		google.maps.event.addListenerOnce( map, 'idle', function() {
-			map.setOptions( { styles : mapStyles } );
-		    document.getElementById( "mapDiv" ).appendChild( loading );
-		});
-	}
+        google.maps.event.addListenerOnce( map, 'idle', function() {
+            map.setOptions( { styles : mapStyles } );
+            document.getElementById( "mapDiv" ).appendChild( loading );
+        });
+    }
 
-	function dataLoadError(){
-		//display a useful error message
-		window.onerror = function(msg, url, linenumber) {
-    	alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
-    	return true;
-		}
-	}
+    function dataLoadError() {
+        //display a useful error message
+        window.onerror = function(msg, url, linenumber) {
+        alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+        return true;
+        }
+    }
 
-	//public methods
-	return {
+    //public methods
+    return {
 
-		init : function(params){
-			var self = this;
-			host = params.host;
-			token = params.token;
+        init : function(params) {
+            var self = this;
+            host = params.host;
+            token = params.token;
 
-			FMEServer.init({
-				server : host,
-				token : token
-			});
+            FMEServer.init({
+                server : host,
+                token : token
+            });
 
-		},
+        },
 
-		requestSchema : function(filePath){
-			fileLocation = filePath
-			var params = 'SourceDataset_SCHEMA=' + filePath;
-			var url = host + '/fmedatastreaming/' + repository + '/' + schemaWorkspace + '?' + params;
-			FMEServer.customRequest(url, 'GET' ,displayNextStep);
-		}, 
+        requestSchema : function(filePath) {
+            fileLocation = filePath
+            var params = 'SourceDataset_SCHEMA=' + filePath;
+            var url = host + '/fmedatastreaming/' + repository + '/' + schemaWorkspace + '?' + params;
+            FMEServer.customRequest(url, 'GET' ,displayNextStep);
+        }, 
 
-		backToUpload : function(){
-			clearDropdowns();
-			document.getElementById('dropdowns').style.display = 'none';
-			document.getElementById('content').style.display = 'block';
-		},
+        backToUpload : function() {
+            clearDropdowns();
+            document.getElementById('dropdowns').style.display = 'none';
+            document.getElementById('content').style.display = 'block';
+        },
 
-		restart : function(){
-			window.location.reload();
-		},
+        restart : function() {
+            window.location.reload();
+        },
 
-		displayMap : function(){
-			var params = getParams();
-			var url = host + '/fmedatastreaming/' + repository + '/'+ geocodeWorkspace + '?' + params + '&token=' + token;
-			
-			initGoogleMap();
+        displayMap : function() {
+            var params = getParams();
+            var url = host + '/fmedatastreaming/' + repository + '/'+ geocodeWorkspace + '?' + params + '&token=' + token;
+            
+            initGoogleMap();
 
-			loading.style.display = 'block';
+            loading.style.display = 'block';
 
-			layer = new google.maps.KmlLayer( url, { 
-				preserveViewport : false,
-				map : map
-			});
+            layer = new google.maps.KmlLayer( url, { 
+                preserveViewport : false,
+                map : map
+            });
 
-			layer.status_changed = function() {
-				if (layer.status = 'FETCH_ERROR'){
-					dataLoadError();
-				}
-				loading.style.display = 'none';
-			};
+            layer.status_changed = function() {
+                if (layer.status = 'FETCH_ERROR'){
+                    dataLoadError();
+                }
+                loading.style.display = 'none';
+            };
 
-			document.getElementById('dropdowns').style.display = 'none';
-			document.getElementById('mapPage').style.display = 'block';
-		}
-	};
+            document.getElementById('dropdowns').style.display = 'none';
+            document.getElementById('mapPage').style.display = 'block';
+        }
+    };
 
 }());
 
